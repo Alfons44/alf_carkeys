efbf315e556d40b51d6cc84eb9d5c8ac948e6ad4
Alfons <39962260+Alfons44@users.noreply.github.com>
2021-05-13 11:57:26 +0200

    Locale fix

--- Alf-carkeys/fxmanifest.lua
+++ Alf-carkeys/fxmanifest.lua
@@ -4,11 +4,13 @@ game 'gta5'
 
 server_script {
 	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
 	'server.lua',
 	'config.lua',
 }
 
 client_script {
+	'@es_extended/locale.lua',
 	'client.lua',
 	'config.lua',
 }
